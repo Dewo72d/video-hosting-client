1c36befe431eaed23a8063e1656592ee7b8218ba
dewo72d <dewo72d@gmail.com>
2023-10-02 13:12:43 +0300

    sort > first new videos

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 
 //Styles
@@ -6,26 +6,27 @@ import styles from "./App.scss";
 
 //Component
 import Card from "../../Containers/Card";
-import {request} from "../../utils/utils";
+import { request } from "../../utils/utils";
 
 //Redux
 import ReactPlayer from "react-player";
-import {useAppSelector} from "../../utils/hooks/redux";
-import {modalSlice} from "../../utils/store/reducers/changeModal";
-import {cardSlice, fetchVideos} from "../../utils/store/reducers/videos";
-import {useDispatch} from "react-redux";
-import {AsyncThunkAction} from "@reduxjs/toolkit";
+import { useAppSelector } from "../../utils/hooks/redux";
+import { modalSlice } from "../../utils/store/reducers/changeModal";
+import { cardSlice, fetchVideos } from "../../utils/store/reducers/videos";
+import { useDispatch } from "react-redux";
+import { AsyncThunkAction } from "@reduxjs/toolkit";
 
 export default function Main() {
 
     const dispatch = useDispatch();
-    const {on} = modalSlice.actions
+    const { on } = modalSlice.actions
 
     const videos = useAppSelector(state => state.videoReducer)
+    const [cards, setCards] = useState([])
 
 
     async function click(video) {
-        dispatch(on({isOpen: true, modal: "video", modalData: {video}}))
+        dispatch(on({ isOpen: true, modal: "video", modalData: { video } }))
     }
 
     async function init() {
@@ -36,12 +37,18 @@ export default function Main() {
         void init();
     }, []);
 
+    useEffect(() => {
+        if (videos.cards.length > 0) {
+            const arr = [...videos.cards].reverse();
+            return setCards(arr)
+        }
+    }, [videos])
 
 
     return (
         <div className={styles.App}>
             <div className={styles.cards}>
-                {videos?.cards?.length > 0 && videos?.cards?.map((el) => (
+                {cards.length > 0 && cards.map((el) => (
                     <Card
                         video={el.video}
                         id={el.id}
